1d39e792a8a311fd473f9c222528f93a21cc7b54
stakervali <stakervali@yandex.com>
2022-06-07 11:13:43 +0300

    added theme button

--- client/src/views/navbar-menu.js
+++ client/src/views/navbar-menu.js
@@ -10,15 +10,20 @@ const staticRoot = process.env.STATIC_ROOT || ''
 export default ({ t, theme, page }) =>
 
 <div className="main-nav-container">
-  <ul className="main-nav">
-    { items && Object.entries(items).map(([ name, url ]) =>
-        <li id={name.replace(/ /g, '')} className={`nav-item ${name == active ? 'active' : ''}`}>
-          <a className="nav-link" href={url} rel="external">
-            <span><img className="menu-logo" alt="" src={`${staticRoot}img/icons/${name.replace(/ /g, '')}-menu-logo.svg`} /></span>
-            <span>{t(name)}</span>
-          </a>
-        </li>
-    ) }
-  </ul>
-    { process.env.NAVBAR_HTML ? navToggle(t, theme, page) : "" }
+    { process.browser ? <div className="switch-theme-icon toggle-theme"> </div>
+        : <a href={page.pathname.substr(1) + updateQuery(page.query, { theme: otherTheme[theme] })} className="switch-theme-icon"></a>
+    }
+    {/*<a href={page.pathname.substr(1) + updateQuery(page.query, { theme: otherTheme[theme] })} className="switch-theme-icon">asd</a>*/}
+
+  {/*<ul className="main-nav">*/}
+  {/*  { items && Object.entries(items).map(([ name, url ]) =>*/}
+  {/*      <li id={name.replace(/ /g, '')} className={`nav-item ${name == active ? 'active' : ''}`}>*/}
+  {/*        <a className="nav-link" href={url} rel="external">*/}
+  {/*          <span><img className="menu-logo" alt="" src={`${staticRoot}img/icons/${name.replace(/ /g, '')}-menu-logo.svg`} /></span>*/}
+  {/*          <span>{t(name)}</span>*/}
+  {/*        </a>*/}
+  {/*      </li>*/}
+  {/*  ) }*/}
+  {/*</ul>*/}
+  {/*  { process.env.NAVBAR_HTML ? navToggle(t, theme, page) : "" }*/}
   </div>
